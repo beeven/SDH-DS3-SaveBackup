275d6c035903747503560cc182d6b6314b14067e
Beeven <beeven@hotmail.com>
2022-10-26 01:04:01 +0000

    Added slot selection

--- main.py
+++ main.py
@@ -1,18 +1,79 @@
+from genericpath import isfile
 import logging
+import os.path
+import shutil
+import json
+import datetime
 
-logging.basicConfig(filename="/tmp/template.log",
-                    format='[Template] %(asctime)s %(levelname)s %(message)s',
+logging.basicConfig(filename="/tmp/sdh-ds3-savebackup.log",
+                    format='%(asctime)s %(levelname)s %(message)s',
                     filemode='w+',
                     force=True)
 logger=logging.getLogger()
 logger.setLevel(logging.INFO) # can be changed to logging.DEBUG for debugging issues
 
+SAVE_FILE_PATH = "/home/deck/.local/share/Steam/steamapps/compatdata/374320/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsIII/0110000100f9e486/"
+SAVE_FILE_NAME = "DS30000.sl2"
+CONFIG_FILE_NAME = "ds3-savebackup.json"
+MAX_SLOTS = 3
+
+INITIALIZED = False
+
+config = {}
+
+def _load_config():
+    global config
+    try:
+        with open(SAVE_FILE_PATH + CONFIG_FILE_NAME, encoding="utf-8") as f:
+            config = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        config = { 
+            "selectedSlot": 1,
+            "slots": [] }
+        for i in range(MAX_SLOTS):
+            config["slots"].append({"lastModified": None})
+            
+def _save_config():
+    with open(SAVE_FILE_PATH + CONFIG_FILE_NAME, "w", encoding="utf-8") as f:
+        json.dump(config, f, ensure_ascii=False)
+
+
 class Plugin:
-    # A normal method. It can be called from JavaScript using call_plugin_function("method_1", argument1, argument2)
-    async def add(self, left, right):
-        return left + right
+
+    async def select_slot(self, slot):
+        if slot is None:
+            slot = 1
+        config["selectedSlot"] = slot
+        logger.info("select slot: %d", slot)
+        return slot
+    
+    async def get_slot_config(self):
+        logger.info("get slot config: %s", json.dumps(config))
+        return config
+
+    async def save_load(self, is_save):
+        if len(config) == 0:
+            _load_config()
+        slot = config["selectedSlot"]
+        backup = SAVE_FILE_NAME+"."+str(slot)+".bak"
+        if is_save:
+            shutil.copy(SAVE_FILE_PATH+SAVE_FILE_NAME, SAVE_FILE_PATH+backup)
+            logger.info("Save %d backup completed.", slot)
+            config["selectedSlot"] = slot
+            config["slots"][slot-1]["lastModified"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            _save_config()
+            return "Backup completed."
+        else:
+            if os.path.isfile(SAVE_FILE_PATH+backup):
+                shutil.copy(SAVE_FILE_PATH+backup, SAVE_FILE_PATH+SAVE_FILE_NAME)
+                logger.info("Save %d recovered.", slot)
+                return "Restore completed."
+            else:
+                logger.info("Save %d is empty.", slot)
+                return f"{slot} is empty."
+
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
-        logger.info("Hello World!")
-        pass
+        logger.info("Dard Souls 3 Save Backup Plugin is running.")
+        _load_config()
--- src/index.tsx
+++ src/index.tsx
@@ -1,105 +1,158 @@
 import {
   ButtonItem,
   definePlugin,
-  DialogButton,
-  Menu,
-  MenuItem,
+  //DialogButton,
+
   PanelSection,
   PanelSectionRow,
-  Router,
+  //Router,
   ServerAPI,
-  showContextMenu,
   staticClasses,
+  DropdownItem,
+  DropdownOption
 } from "decky-frontend-lib";
-import { VFC } from "react";
-import { FaShip } from "react-icons/fa";
+import { VFC, useState, useEffect, useMemo } from "react";
+import { FaDragon } from "react-icons/fa";
 
-import logo from "../assets/logo.png";
+//import logo from "../assets/logo.png";
 
-// interface AddMethodArgs {
-//   left: number;
-//   right: number;
-// }
+interface BackupMethodArgs {
+  is_save: boolean;
+}
 
-const Content: VFC<{ serverAPI: ServerAPI }> = ({}) => {
-  // const [result, setResult] = useState<number | undefined>();
+interface SlotConfig {
+  selectedSlot: number;
+  slots: SlotInfo[]
+}
+interface SlotInfo {
+  lastModified: string;
+}
 
-  // const onClick = async () => {
-  //   const result = await serverAPI.callPluginMethod<AddMethodArgs, number>(
-  //     "add",
-  //     {
-  //       left: 2,
-  //       right: 2,
-  //     }
-  //   );
-  //   if (result.success) {
-  //     setResult(result.result);
-  //   }
-  // };
+const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
+  const [statusText, setStatusText] = useState<string | undefined>();
+
+  const onSaveLoad = async (save: boolean) => {
+    const result = await serverAPI.callPluginMethod<BackupMethodArgs, string>(
+      "save_load",
+      {
+        is_save: save,
+      }
+    );
+    if (result.success) {
+      setStatusText(result.result);
+      const configresult = await serverAPI.callPluginMethod<any,SlotConfig>("get_slot_config",{});
+      if(configresult.success)
+      {
+        setSlotConfig(configresult.result);
+      }
+    }
+  };
+
+  const [slotConfig, setSlotConfig] = useState<SlotConfig | null>(null);
+  const [selectedSlot, setSelectedSlot] = useState<number | null>(null);
+
+  const onSelectionChange = async (slot: number) =>{
+    setSelectedSlot(slot);
+    await serverAPI.callPluginMethod("select_slot",{ slot: slot })
+  }
+
+  useEffect(()=>{
+  serverAPI.callPluginMethod<any,SlotConfig>("get_slot_config", {}).then(result =>{
+    if(result.success) {
+      setSlotConfig(result.result);
+      setSelectedSlot(result.result.selectedSlot);
+    }
+  });
+}, []);
+
+  const slotOptions = useMemo(
+    (): DropdownOption[] => {
+      let slotCount = slotConfig?.slots?.length;
+      if(slotCount != null && typeof(slotCount) != 'undefined') {
+        let slots = [];
+        for(let i=0; i<slotCount; i++)
+        {
+          let timestamp = slotConfig?.slots[i]?.lastModified;
+          if(timestamp === null || typeof(timestamp) === "undefined")
+          {
+            timestamp = "Empty"
+          }
+          let entry = {data: i+1, label: (i+1).toString() + " " +  timestamp};
+          slots.push(entry)
+        }
+        return slots;
+      }
+      else {
+        return [
+          { data: 1, label: "1 Empty"},
+          { data: 2, label: "2 Empty"},
+          { data: 3, label: "3 Empty"},
+        ]
+      }
+    },
+    [slotConfig]
+  );
+  
 
   return (
-    <PanelSection title="Panel Section">
+    <PanelSection title="Save Backup">
       <PanelSectionRow>
         <ButtonItem
           layout="below"
-          onClick={(e) =>
-            showContextMenu(
-              <Menu label="Menu" cancelText="CAAAANCEL" onCancel={() => {}}>
-                <MenuItem onSelected={() => {}}>Item #1</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #2</MenuItem>
-                <MenuItem onSelected={() => {}}>Item #3</MenuItem>
-              </Menu>,
-              e.currentTarget ?? window
-            )
-          }
+          onClick={() => onSaveLoad(true)}
         >
-          Server says yolo
+          Save
         </ButtonItem>
-      </PanelSectionRow>
-
-      <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <img src={logo} />
-        </div>
       </PanelSectionRow>
 
       <PanelSectionRow>
         <ButtonItem
           layout="below"
-          onClick={() => {
-            Router.CloseSideMenus();
-            Router.Navigate("/decky-plugin-test");
-          }}
+          onClick={() => onSaveLoad(false)}
         >
-          Router
+          Load
         </ButtonItem>
+      </PanelSectionRow>
+      <PanelSectionRow>
+        <div>{statusText}</div>
+      </PanelSectionRow>
+      <PanelSectionRow>
+        <DropdownItem 
+          label = "Save Slot #"
+          menuLabel="Slot #"
+          strDefaultLabel="Select a slot"
+          rgOptions={slotOptions}
+          selectedOption = {selectedSlot}
+          onChange={(data) => onSelectionChange(data.data)}
+        />
       </PanelSectionRow>
     </PanelSection>
   );
 };
 
-const DeckyPluginRouterTest: VFC = () => {
-  return (
-    <div style={{ marginTop: "50px", color: "white" }}>
-      Hello World!
-      <DialogButton onClick={() => Router.NavigateToStore()}>
-        Go to Store
-      </DialogButton>
-    </div>
-  );
-};
+// const DeckyPluginRouterTest: VFC = () => {
+//   return (
+//     <div style={{ marginTop: "50px", color: "white" }}>
+//       Hello World!
+//       <DialogButton onClick={() => Router.NavigateToStore()}>
+//         Go to Store
+//       </DialogButton>
+//     </div>
+//   );
+// };
 
 export default definePlugin((serverApi: ServerAPI) => {
-  serverApi.routerHook.addRoute("/decky-plugin-test", DeckyPluginRouterTest, {
-    exact: true,
-  });
+  // serverApi.routerHook.addRoute("/decky-plugin-test", DeckyPluginRouterTest, {
+  //   exact: true,
+  // });
+
 
   return {
-    title: <div className={staticClasses.Title}>Example Plugin</div>,
+    title: <div className={staticClasses.Title}>DS3 SaveBackup</div>,
     content: <Content serverAPI={serverApi} />,
-    icon: <FaShip />,
+    icon: <FaDragon />,
     onDismount() {
-      serverApi.routerHook.removeRoute("/decky-plugin-test");
+      //serverApi.routerHook.removeRoute("/decky-plugin-test");
     },
   };
 });
